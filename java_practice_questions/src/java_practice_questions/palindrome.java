3094d7fa204b156e11d17dc22bc7e4953921997f
riyajawa <64279936+riyajawa@users.noreply.github.com>
2020-11-05 17:13:28 +0530

    palindrome.java

---
+++ java_practice_questions/src/java_practice_questions/palindrome.java
@@ -0,0 +1,32 @@
+package java_practice_questions;
+import java.util.*;
+
+public class palindrome {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		
+		int n=sc.nextInt();
+		
+		int r,sum=0,i,temp;
+		
+		temp=n;
+		while(n>0)
+		{
+			r=n%10;
+			sum=(sum*10)+r;
+			n=n/10;
+		}
+		if(sum==temp)
+		{
+			System.out.println("yes");
+		}
+		else 
+		{
+			System.out.println("no");
+		}
+
+	}
+
+}
