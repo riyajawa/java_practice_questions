25c15b4dab0fb70d5c9bb0b4b5e65014bd98f4aa
riyajawa <64279936+riyajawa@users.noreply.github.com>
2020-11-05 18:11:58 +0530

    factorial.java

---
+++ java_practice_questions/src/java_practice_questions/factorial.java
@@ -0,0 +1,19 @@
+package java_practice_questions;
+import java.util.*;
+
+public class factorial {
+
+	public static void main(String[] args) {
+		
+		int fact=1,i;
+		Scanner sc= new Scanner(System.in);
+		int n=sc.nextInt();
+		for(i=n;i>05;i--)
+		{
+			fact=fact*i;
+		}
+		System.out.println(fact);
+
+	}
+
+}
